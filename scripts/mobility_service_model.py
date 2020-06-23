d9d19813fc818f6dcc6d484283e0afbc29384c64
doorleyr <doorleyr@tcd.ie>
2020-06-23 14:51:43 +0100

    minor

--- scripts/mobility_service_model.py
+++ scripts/mobility_service_model.py
@@ -256,12 +256,12 @@ class MobilityModel():
         print('Updating Simulation')
         self.geogrid_data=geogrid_data
         self.update_grid()
-        print(len(self.pop.base_sim))
-        print(len(self.pop.new))
-        print(len(self.pop.floating))
-        print(len(self.pop.vacant))
         self.activity_scheduler.find_locations_for_activities(self)
         self.create_new_agents()
+        print(len(self.pop.base_sim))
+        print(len(self.pop.new))
+        print(len(self.pop.base_floating))
+        print(len(self.pop.base_vacant))
         self.hlc.home_location_choices(self.pop)
         new_sim_pop=[]
         for p in self.pop.new + self.pop.base_floating:
